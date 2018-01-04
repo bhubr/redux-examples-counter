ae33aaa3dd556c4cd5d30d82e0602eddcd3a7d3b
Benoît Hubert <benoithubert@gmail.com>
2018-01-04 10:33:19 +0100

    Use a placeholder in case the movie has no poster image

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class MovieItem extends React.Component {
   render() {
     const { title, poster, overview } = this.props;
-    const posterUrl = 'https://image.tmdb.org/t/p/w500_and_h281_bestv2' + poster;
+    const posterUrl = poster ? 'https://image.tmdb.org/t/p/w500_and_h281_bestv2' + poster :
+      'http://via.placeholder.com/500x281?text=' + title.replace(' ', '+');
     return (
       <div className="media media-box">
         <img className="mr-3" src={posterUrl} alt={title} />
